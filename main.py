56f84b2d6fd53b41de8092a629e01b25dc453aed
Sumra Jabeen <simrajabeen16@gmail.com>
2025-05-08 15:36:29 +0500

    first commit

--- main.py
+++ main.py
@@ -10,11 +10,3 @@ def greeting():
 
     return {"message": "Hello, World from Simra!"}
 
-
-# echo "# Generative_AI_Quarter_04" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin https://github.com/Coderhubs/Generative_AI_Quarter_04.git
-# git push -u origin main
